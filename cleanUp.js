7dbc7aaa11a65923a531316b7e0b0b1eb3869be7
Seth Burns <seth@sethburns.com>
2023-02-12 11:25:25 -0800

    Finished cleanUp.js

--- cleanUp.js
+++ cleanUp.js
@@ -4,41 +4,52 @@
   // Modify the code to make that improvement(s)
   // write a JS comment to explain what you changed, and why
   // make sure the code you submit WORKS - you can run this entire file 
-    // using `node cleanUp.js` or copy and paste into a replit
+  // using `node cleanUp.js` or copy and paste into a replit
 
 
 // EX 1:
-function askForName() { console.log("Hello, what is your name?") }
+function askForName() { 
+  console.log("Hello, what is your name?") 
+}
 
 askForName()
+
+// I put the console log indented on a new line of code and moved the last curly to the third line.
 
 
 // EX 2:
 function addThreeNums(first, second, third) {
-var sum = first + second + third
-return sum;
-      }
+  var sum = first + second + third
+  return sum;
+}
 
-addThreeNums(1, 2, 3);
-addThreeNums(4, 2, 7);
+console.log(addThreeNums(1, 2, 3));
+console.log(addThreeNums(4, 2, 7));
+
+// I fixed indentation issues to make it more readable.
+// added console.log before the function to read it in the log.
 
 
 // EX 3:
-func makeFreshPesto(){
+function makeFreshPesto() {
   console.log("Buy ingredients: basil, parmesan, romano, olive oil, pine nuts, garlic, salt, pepper");
   console.log("Pulse basil and pine nuts");
   console.log("Add garlic and cheeses");
   console.log("Slowly pour in oil");
-  console.log("Season");    }
+  console.log("Season");    
+}
 
 makeFreshPesto();
 
+// I moved the last curly to the final line of code and finished the word function as func is not something that works. 
+// Added space before first curly.
 
 //  EX 4:
-function average(num1, num2) 
-  {
-var sum = num1 + num2;
-    var avg = sum / 2;
+function average(num1, num2) {
+  var sum = num1 + num2;
+  var avg = sum / 2;
+  return avg
+}
 
-return avg
-  }
+console.log(average(2, 5))
+// I fixed indentation issues, extra empty lines of code, and made it more readable. Added a console.log for the function.
